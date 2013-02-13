e77157b681ccb3354b314c46a85300eb239dd26c
Brian Ambielli <brianambielli2007@u.northwestern.edu>
2013-02-13 16:32:32 -0600

    heroku with changes

--- app/mailers/no_pay.rb
+++ app/mailers/no_pay.rb
@@ -2,8 +2,7 @@ class NoPay < ActionMailer::Base
   default from: "getwellbe@gmail.com"
 
   def nopayNotification
-  	mail(:to => "brianambielli2007@u.northwestern.edu", :subject => "Someone just signed up on NOPAY site")
+  	mail(:to => "brianambielli2007@u.northwestern.edu, anolan2013@kellogg.northwestern.edu, sjani2013@kellogg.northwestern.edu", :subject => "Someone just signed up on NOPAY site")
 end
 end
 
-#anolan2013@kellogg.northwestern.edu, sjani2013@kellogg.northwestern.edu
